c3dd1542947a8be84b458cc14cf0bd5a3237ae9a
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-15 19:47:10 +0700

    ww

--- Project/Assets/Scripts/SceneSelection.cs
+++ Project/Assets/Scripts/SceneSelection.cs
@@ -5,11 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+[System.Serializable]
+public class MeangButton
+{
+	public Button _button;
+	public int sceneID;
+}
+
+
+
 public class SceneSelection : MonoBehaviour
 {
 	int ScenesUnlocked;
-	public Button[] buttons;
+	public MeangButton[] buttons;
+
 	// dddddddddddddd
+	[Header("debug")]
 	[SerializeField] TMP_Text mainText;
 
 	void Start () 
@@ -19,27 +30,21 @@ public class SceneSelection : MonoBehaviour
 		// dddddddddddddd
 		mainText.SetText(ScenesUnlocked.ToString());
 
-		foreach (Button _b in buttons)
+		foreach (MeangButton _b in buttons)
 		{
-			_b.interactable = false;
+			_b._button.interactable = false;
+
+			if(_b.sceneID <= ScenesUnlocked)
+			{
+				_b._button.interactable = true;
+			}
 		}
+		
 		// for (int i = 0; i < buttons.Length; i++)
 		// {
 		// 	buttons[i].interactable = false;
 		// }
 
-		for (int i = 0; i < buttons.Length; i++)
-		{
-			if(!(i < ScenesUnlocked))
-			{
-				return;
-			}
-			else
-			{
-				buttons[i].interactable = true;
-			}
-		}
-		
 		// for (int i = 0; i < ScenesUnlocked; i++)
 		// {
 		// 	buttons[i].interactable = true;
